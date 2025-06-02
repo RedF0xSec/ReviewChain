4a09d6b76f4877809d883fa62e41e56450e62919
LuDonnarumma <150720960+LuDonnarumma@users.noreply.github.com>
2025-06-02 10:12:22 +0200

    Modifica

--- deploy.js
+++ deploy.js
@@ -1,6 +1,11 @@
 const { Web3 } = require('web3');
 const fs = require('fs');
 
+
+// COMMENTO PAX
+
+const ciao = "Ciao, sono un commento di test per il deploy.js";
+
 // Connect to Ganache
 const web3 = new Web3('http://localhost:7545');
 
