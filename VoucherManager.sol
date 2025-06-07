bc5d40bfe374c3fb476820627a19798cf6423470
LuDonnarumma <150720960+LuDonnarumma@users.noreply.github.com>
2025-06-07 15:03:23 +0200

    Completata classe voucher da vedere insieme

--- VoucherManager.sol
+++ VoucherManager.sol
@@ -12,7 +12,11 @@ struct Voucher {
 }
 
 // Qui va aggiunto qualcosa che possa far capire se è stato già usato o no
+// voglio mettere un booleano che mi dica se è stato usato o no, ma non so se è una buona idea
+
+
 contract VoucherManager {
+    address public owner_contract; // Proprietario del contratto
     uint256 public counter; //per l'id progressivo dei voucher
     mapping(uint256 => Voucher) public vouchers;
     address public actorRegistry;
@@ -21,11 +25,38 @@ contract VoucherManager {
     
     constructor(address _actorRegistry) {
         actorRegistry = _actorRegistry;
+        counter = 1; // Inizializza il contatore a zero
+        owner_contract = msg.sender; // Imposta il proprietario del contratto
+    }
+
+    // Modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner_contract, "Not contract owner");
+        _;
     }
 
     // Funzione interna per emettere un voucher
-    function emitVoucher(address customer, address restaurant, uint256 discount, string memory metadataURI) external {
-    
+    function emitVoucher(address customer, address restaurant, uint256 discount, string memory metadataURI) external onlyOwner() {
+
+        require(ActorRegistry(actorRegistry).verifySeller(restaurant), "Restaurant not registered");
+        require(customer != address(0), "Invalid customer address");
+        require(restaurant != address(0), "Invalid restaurant address");
+        require(discount > 0, "Discount must be greater than zero");
+
+        // Incrementa il contatore per ottenere un nuovo id
+        counter++;
+
+        // Crea il nuovo voucher
+        vouchers[counter] = Voucher({
+            id: counter,
+            owner: customer,
+            restaurant: restaurant,
+            discount: discount,
+            metadataURI: metadataURI
+        });
+
+        // Emetti l'evento
+        emit VoucherEmitted(counter, customer, restaurant, discount, metadataURI);
     }
 
     // Funzione getter
